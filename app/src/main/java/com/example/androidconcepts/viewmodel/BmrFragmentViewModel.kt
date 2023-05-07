3059cbbc2ffe4e72293b868ca5e99ad373ebd300
Nity <nitysingh27@gmail.com>
2023-05-07 17:55:57 +0530

    Bmr Repository

--- app/src/main/java/com/example/androidconcepts/viewmodel/BmrFragmentViewModel.kt
+++ app/src/main/java/com/example/androidconcepts/viewmodel/BmrFragmentViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class BmrFragmentViewModel @Inject constructor(
     private var bmrRepository: BmrRepository
 ) : ViewModel() {

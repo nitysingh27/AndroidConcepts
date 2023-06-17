cd02b26c49be96ab8c0a896b691af66c4489e8ba
nitysingh27 <70686691+nitysingh27@users.noreply.github.com>
2023-06-17 15:21:53 +0530

    Update Application.kt

--- app/src/main/java/com/example/androidconcepts/Application.kt
+++ app/src/main/java/com/example/androidconcepts/Application.kt
@@ -8,4 +8,19 @@ class MyApplication : Application() {
   
   //https://teams.microsoft.com/l/meetup-join/19%3ameeting_MzcxZjQ4ZjYtZjQ5NS00NTYzLWEwNDMtMjI4YzdiOTM4ODU1%40thread.v2/0?context=%7b%22Tid%22%3a%22cc55ca6d-1316-47a9-8cb2-a791739443e6%22%2c%22Oid%22%3a%222b9b7968-48ae-44d0-848d-b6b0bd1320d5%22%7d
 //https://cocubes.in/onebancassessment
-}
+
+
+
+//JUST COMPLETED
+ 
+Assessment
+OneBanc | CAST | 2021
+ 
+Time Taken
+14 minutes 54 seconds
+ 
+Submitted On
+17 Jun, 2023 03:21 PM
+ 
+Candidate ID
+38278881}
